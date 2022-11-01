f298002635993c6464befc27a2cbfd016322dc67
“otogi <o.yuzurihara@lincraft.co.jp>
2022-11-01 17:23:50 +0900

    一旦commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ import { SignUp } from './features/Auth/SignUp.js';
 import { ResetPassword } from './features/Auth/ResetPassword.js';
 import { ConfirmSignUp } from './features/Auth/ConfirmSignUp.js';
 import { MainView } from './features/View/MainView.js';
+import { Routes, Route, Link, NavLink, BrowserRouter } from 'react-router-dom';
+import { Home } from './routes/Home.js';
+import { TmpView } from './routes/TmpView.js';
+import { NotFound } from './routes/NotFound.js';
+import { Posts } from './routes/Posts.js';
+import { Post } from './routes/Post.js';
 
 function App() {
 
@@ -16,14 +22,34 @@ function App() {
   I18n.setLanguage("ja");
 
   return (
-    <AmplifyAuthenticator>
-      <SignIn />
-      <ForgotPassword />
-      <SignUp />
-      <ResetPassword />
-      <ConfirmSignUp />
-      <MainView />
-    </AmplifyAuthenticator>
+    <>
+      {/* <AmplifyAuthenticator>
+        <SignIn />
+        <ForgotPassword />
+        <SignUp />
+        <ResetPassword />
+        <ConfirmSignUp />
+        <MainView />
+      </AmplifyAuthenticator> */}
+      <BrowserRouter>
+        <h1>メモ帳（←は共通表示）</h1>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/Posts">Posts</Link></li>
+            <li><Link to="/TmpView">TmpView</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route index element={<Home message='This is Home' />} />
+          <Route path="/Posts" element={<Posts />}>
+            <Route path="Post" element={<Post />} />
+          </Route >
+          <Route path="/TmpView" element={<TmpView />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/routes/TmpView.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function TmpView(){
+    return <h2>TmpView</h2>;
+}

3e55afe4d1a355442822f918035dc2fd22a3c70b
“otogi <o.yuzurihara@lincraft.co.jp>
2022-11-02 15:42:04 +0900

    commit TRAINING-44

--- src/features/View/Note.js
+++ src/features/View/Note.js
@@ -6,8 +6,17 @@ export function Note(){
   const location = useLocation();
   // location.stateはいくつかのプロパティの集まり（現状、descriptionのみ）
   // {xxx}と指定することでプロパティ：xxxの値を取得できる
-  const {description} = location.state;
-  
-  return <p>{description}</p>;
+  // const {description, createdAt, updatedAt} = location.state;
+  const {description, createdAt, updatedAt} = location.state;
+  const fixedCreatedAt = new Date(createdAt).toLocaleString()
+  const fixedUpdatedAt = new Date(updatedAt).toLocaleString()
+
+  return(
+    <div>
+      <p>内容： {description}</p>
+      <p>作成日時： {fixedCreatedAt}</p>
+      <p>更新日時： {fixedUpdatedAt}</p>
+    </div>
+  );
 
 }
